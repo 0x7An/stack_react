a18bdb54784a446b6e3047880dd3874556ed070f
anderson da silva lima <primataxdeus@gmail.com>
2018-05-04 16:26:57 -0300

    scratching first components

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h1 className="App-title">Stack overflow</h1>
+      </header>
+    )
+  }
+};
---
+++ src/components/subheader/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Subheader extends Component {
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+};
