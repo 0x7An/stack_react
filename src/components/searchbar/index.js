2e78d142d23325d981545c817118dffe7bea2e33
anderson da silva lima <primataxdeus@gmail.com>
2018-05-05 15:02:05 -0300

    binding state to searchbar :hammer:

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,50 +1,86 @@
 import React, { Component } from 'react';
 import { Button, Panel } from 'react-bootstrap';
 
-export default class parameters extends Component {
+export default class Searchbar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        parameters: {
+            score: 1000,
+            sort: "activity",
+            limit: 10,
+            tag: ""
+        }
+    };
+  
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  };
+  
+  onInputChange(event){
+    let values = this.state.parameters;
+    values[event.target.name] = event.target.value;
+    this.setState({parameters: values});
+  }
+
+  onFormSubmit(event){
+    event.preventDefault();
+    console.log(this.state.parameters);
+  }
 
   render() {
     return (
-
         <Panel>
           <Panel.Heading>
             <Panel.Title componentClass="h3">Buscar na API</Panel.Title>
           </Panel.Heading>
-          
-          <Panel.Body>  
-              <div style={styles.parameters}>
-                <input 
-                  style={styles.parameter}
-                  type="text" 
-                  placeholder="tag">
-                  
-                </input>
-                <input 
-                  style={styles.parameter}
-                  type="text" 
-                  placeholder="limit">
 
-                </input>
-                <input 
-                  style={styles.parameter}
-                  type="text" 
-                  placeholder="score">
-
-                </input>
+          <Panel.Body>            
+            <div style={styles.parameters}>
                 <input
-                  style={styles.parameter} 
-                  type="text" 
-                  placeholder="sort">
-                </input>
-              </div>
+                  name="tag"
+                  style={styles.parameter}
+                  placeholder="tag"
+                  className="form-control"
+                  onChange={this.onInputChange}
+                  value={this.state.parameters.tag}
+                />
 
-              <Button 
+                <input
+                  name="score"
+                  style={styles.parameter}
+                  placeholder="score"
+                  className="form-control"
+                  value={this.state.parameters.score}
+                  onChange={this.onInputChange}
+                />
+
+                <input
+                  name="sort"
+                  style={styles.parameter}
+                  placeholder="sort"
+                  className="form-control"
+                  value={this.state.parameters.sort}
+                  onChange={this.onInputChange}
+                />
+
+                <input
+                  name="limit"
+                  style={styles.parameter}
+                  placeholder="limit"
+                  className="form-control"
+                  value={this.state.parameters.limit}
+                  onChange={this.onInputChange}
+                />               
+              </div>
+              <Button
+                onClick={this.onFormSubmit}
                 bsStyle="success"
-                style={styles.button}
-              >
+                style={styles.button}>
                 Buscar
               </Button>
-
           </Panel.Body>
         </Panel>
     )
@@ -68,7 +104,7 @@ const styles = {
   },
   button:{
    justifyContent: 'flex-end',
-  //  alignSelf: 'flex-end',
+   alignSelf: 'flex-end',
    fontSize: 14,
    marginRight: 15
   }
