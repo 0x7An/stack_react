17241c61f24d728cab9e773adcae85b0eaa1b4fa
anderson da silva lima <primataxdeus@gmail.com>
2018-05-06 13:42:00 -0300

    Selecting the corret fileds for root search

--- src/data/rootquery.js
+++ src/data/rootquery.js
@@ -3,11 +3,13 @@ import gql from "graphql-tag";
 const ROOT_QUERY = gql`
 {
   questions(tags:"javascript"){
-    score,
     question_id,
+    creation_date,
+    title,
+    score,
     tags,
     owner{
-          user_id
+      display_name
     }
   }
 }
